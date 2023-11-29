fc4879dd35df36f75028cdef9f6f708bfec26fc5
Alvin C <122063397+ajdhasdklj@users.noreply.github.com>
2023-11-28 17:55:09 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
-const express = require("express"); 
-// instantiates Express
+const express = require('express');
+const path = require('path');
+
 const app = express();
-// for serving files in the public folder, such as CSS files
-app.use(express.static("./public"));
-// server responds when the client sends a GET request for the / endpoint
 
-app.get("/", function(req,res) {
-    res.sendFile(__dirname + "/index.html");
-});
-app.post("/", function(req,res) {
-     res.sendFile(__dirname + "/index2.html");
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Route for serving the HTML file
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-
-app.listen(process.env.PORT || 3000, function() {
-    console.log(`Server is running on port ${process.env.PORT}.`);
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
